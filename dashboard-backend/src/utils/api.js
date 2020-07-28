7d9d2f204feeca8329c05d6aed29bb4efca62a3e
JHackett <58866873+James-Hackett@users.noreply.github.com>
2020-07-28 01:00:23 +0100

    :bug: Fix: fixes error code 401

--- dashboard-backend/src/utils/api.js
+++ dashboard-backend/src/utils/api.js
@@ -5,7 +5,7 @@ async function getBotGuilds() {
   const response = await fetch("http://discord.com/api/v6/users/@me/guilds", {
     method: 'GET',
     headers: {
-      Authorisation: `Bot ${TOKEN}`
+      Authorization: `Bot NzA4NDA4NzMyNTc2NTE0MDky.XrW7Cw.ie2QEg4I-hwElVxphPHo_0z1qi4`
     }
   });
   return response.json();

921b3eacd87cb07d7e983da55902a35c8f8eb704
JHackett <58866873+James-Hackett@users.noreply.github.com>
2020-07-28 02:13:43 +0100

    :sparkles: Feat: displays mutual guilds on FE

--- dashboard-backend/src/routes/discord.js
+++ dashboard-backend/src/routes/discord.js
@@ -1,12 +1,19 @@
 const router = require('express').Router();
 const { getBotGuilds } = require('../utils/api');
+const { getMutualGuilds } = require('../utils/utils')
+const User = require('../database/schemas/User')
 
-router.get('/guilds', async (req, res) => {
+router.get('/common-guilds', async (req, res) => {
   const guilds = await getBotGuilds();
-  console.log(guilds);
-  res.send(guilds);
+  const user = await User.findOne({ discordId: req.user.discordId });
+  if (user) {
+    const userGuilds = user.get('guilds');
+    const mutualGuilds = getMutualGuilds(userGuilds, guilds);
+    res.send(mutualGuilds);
+  } else {
+    return res.status(401).send({ msg: 'Unauthorized' });
+  }
+
 });
-
-
 
 module.exports = router;
